339a3e59346ad305feaea4409b2507027a0efe0b
Vasyl Boyko <vboiko0@gmail.com>
2023-02-08 19:39:44 +0200

    add env

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // put your google client id here
-const GOOGLE_CLIENT_ID = "";
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
